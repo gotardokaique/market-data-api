2e7367dba0b0345a064f80f808f34e432b213ec1
kaiquecgotardo <kaiquecgotardo@gmail.com>
2026-02-19 10:33:59 -0400

    Initial Spring Boot project structure

---
+++ src/main/java/com/kaique/marketdata/MarketDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.kaique.marketdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaique/marketdata/MarketDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.marketdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
