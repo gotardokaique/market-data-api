bdedf48c6358b32007bf73cd5606599468e35263
kaiquecgotardo <kaiquecgotardo@gmail.com>
2026-02-19 11:14:22 -0400

    fix: ajustes iniciais para startar

--- src/main/java/com/kaique/marketdata/MarketDataApiApplication.java
+++ src/main/java/com/kaique/marketdata/MarketDataApiApplication.java
@@ -2,8 +2,9 @@ package com.kaique.marketdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MarketDataApiApplication {
 
 	public static void main(String[] args) {
